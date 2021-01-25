10f5bdd02bef61885ee86b9d68ced553380cffbb
x4e <x4e_x4e@protonmail.com>
2021-01-25 11:22:02 +0000

    Improve test

--- classes/testing/LookupSwitch.java
+++ classes/testing/LookupSwitch.java
@@ -4,17 +4,17 @@ public class LookupSwitch {
 	public static void main(String[] args) {
 		switch (i) {
 			case -1:
-				throw new IllegalStateException();
+				throw new IllegalStateException("-1");
 			case 0:
 				return;
 			case 1:
-				throw new IllegalStateException();
+				throw new IllegalStateException("1");
 			case 4:
-				throw new IllegalStateException();
+				throw new IllegalStateException("4");
 			case 94132:
-				throw new IllegalStateException();
+				throw new IllegalStateException("94132");
 			default:
-				throw new IllegalStateException();
+				throw new IllegalStateException("default");
 		}
 	}
 }
